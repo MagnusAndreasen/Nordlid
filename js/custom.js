320897cbdb5b0527ee7376a0548faf3afe582aad
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-10-02 00:04:33 +0200

    form working
    got the form working. Also added some styling to the site

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,46 @@
 // JavaScript Document
 
+function myForm()                                    
+{ 
+    var name = document.forms["RegForm"]["fullName"];               
+    var email = document.forms["RegForm"]["emailAddress"];    
+    var phone = document.forms["RegForm"]["mobilePhone"]; 
+	var check = document.forms ["RegForm"] ["permission"];
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    }  
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+}
